9a45980d8b94a63a923fb4875c7615063852703e
morazzela <morazzela@protonmail.com>
2024-08-30 22:58:07 +0200

    wip

--- frontend/src/components/Page/PageHeader.tsx
+++ frontend/src/components/Page/PageHeader.tsx
@@ -12,7 +12,7 @@ export default function PageHeader({ children, hideMenu }: Props) {
     return (
         <div class="border-b px-4 h-16 flex items-center bg-white dark:bg-gray-950">
             <Show when={!hideMenu}>
-                <div onclick={() => setIsOpen(true)} class="ml-2 mr-6">
+                <div onclick={() => setIsOpen(true)} class="ml-2 mr-6 lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
                     </svg>
